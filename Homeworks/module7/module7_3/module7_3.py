d50058522c3786126f5d016f759d51a2271b827b
Mikhail <6406244@gmail.com>
2024-12-20 07:00:07 +0300

    change module7_3

--- Homeworks/module7/module7_3/module7_3.py
+++ Homeworks/module7/module7_3/module7_3.py
@@ -43,5 +43,48 @@ print(finder2.find('TEXT'))
 print(finder2.count('teXT'))
 
 print(finder3.get_all_words())
-print(finder3.find('Need'))
-print(finder3.count('That'))
+print(finder3.find('for'))
+print(finder3.count('MY'))
+
+# class WordsFinder:
+#     def __init__(self, *file_names):
+#         self.file_names = file_names
+#
+#     def get_all_words(self):
+#         all_words = {}
+#         for name in self.file_names:
+#             with open(name, 'r', encoding='utf-8') as file:
+#                 strings = file.readlines()
+#                 strings_list = []
+#                 for string in strings:
+#                     p = [',', '.', '=', '!', '?', ';', ':', ' - ']
+#                     for symbol in p:
+#                         string = string.replace(symbol, '')
+#                     string_without_p = string.lower().split()
+#                     strings_list.extend(string_without_p)
+#                 all_words[name] = strings_list
+#         return all_words
+#
+#     def find(self, word):
+#         all_words = self.get_all_words()
+#         for name, words in all_words.items():
+#             for index, word_from_file in enumerate(words):
+#                 if word.lower() == word_from_file:
+#                     return {name: index + 1}
+#
+#     def count(self, word):
+#         all_words = self.get_all_words()
+#         for name, words in all_words.items():
+#             if word.lower() in words:
+#                 return {name: words.count(word.lower())}
+#
+#
+# finder2 = WordsFinder('test_file.txt')
+# finder3 = WordsFinder('test_file2.txt')
+# print(finder2.get_all_words())
+# print(finder2.find('TEXT'))
+# print(finder2.count('teXT'))
+#
+# print(finder3.get_all_words())
+# print(finder3.find('My'))
+# print(finder3.count('That'))
