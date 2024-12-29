6dd7c9d088d186e64e6c6434cf6c4d08b603ec58
Micky <6406244@gmail.com>
2024-12-29 17:13:03 +0300

    test.py

--- test.py
+++ test.py
@@ -1,19 +1,8 @@
-import numpy as np
+def add_numbers(a: int, b: int) -> int:
+    return a + b
 
-# Функция и её производная
-def f(x):
-    return x**2
 
-def grad_f(x):
-    return 2 * x
-
-# Параметры
-x = 10  # Начальная точка
-learning_rate = 0.1
-iterations = 100
-
-# Градиентный спуск
-for i in range(iterations):
-    gradient = grad_f(x)
-    x = x - learning_rate * gradient
-    print(f"Итерация {i+1}: x = {x:.5f}, f(x) = {f(x):.5f}")
+x: int = True + True
+y: int = 10
+result = add_numbers(x, y)
+print(result)
