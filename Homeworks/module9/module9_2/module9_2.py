88e0357f1ee508a4766cbe2723043b552928162c
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2025-01-02 12:03:44 +0300

    Update module9_2.py

--- Homeworks/module9/module9_2/module9_2.py
+++ Homeworks/module9/module9_2/module9_2.py
@@ -3,10 +3,8 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer',
                   'Assembler']
 
 first_result = [len(string) for string in first_strings if len(string) >= 5]
-second_result = [(x, y) for x in first_strings for y in second_strings if
-                 len(x) == len(y)]
-third_result = {x: len(x) for x in first_strings + second_strings if len(x) %
-                2 == 0}
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {x: len(x) for x in first_strings + second_strings if len(x) % 2 == 0}
 
 print(first_result)
 print(second_result)
